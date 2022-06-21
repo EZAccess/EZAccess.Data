ada56f61e509a23adbb093f1861c6ab3693a2796
EZ Access Gideon <gideon@ezaccess.nl>
2022-06-21 12:05:10 +0200

    21-6-22

--- EZField.cs
+++ EZField.cs
@@ -8,6 +8,33 @@ public class EZField
     private readonly Type _model;
     private readonly string _fieldName;
     private readonly PropertyInfo _fieldInfo;
+    private string? _displayName;           //DisplayAttribute.Name
+    private string? _displayShortName;      //DisplayAttribute.Name
+    private bool? _AllowEdit;               //EditableAttribute.AllowEdit
+    private string? _dataFormatString;      //DisplayFormatAttribute.DataFormatString
+    private string? _nullDisplayText;       //DisplayFormatAttribute.NullDisplayText
+    private bool? _key;                     //KeyAttribute
+    private bool? _isRequired;              //RequiredAttribute
+    private bool? _isCreditCard;            //CreditCardAttribute
+    private bool? _isEmailAddress;          //EmailAddressAttribute 
+    private bool? _isPhone;                 //PhoneAttribute
+    private bool? _hasMaxLength;            //MaxLengthAttribute
+    private int? _maxLength;                //MaxLengthAttribute.Length
+    private bool? _hasMinLength;            //MinLengthAttribute
+    private int? _minLength;                //MixLengthAttribute.Length
+    private bool? _hasRange;                //RangeAttribute
+    private object? _rangeMaximum;          //RangeAttribute.Maximum
+    private object? _rangeMinimum;          //RangeAttribute.Minimum
+    private Type? _rangeType;               //RangeAttribute.OperandType
+    private bool? _hasRegularExpression;     //RegularExpressionAttribute
+    private string? _regularExpression;     //RegularExpressionAttribute.Pattern
+    private bool? _scaffoldColumn;          //ScaffoldColumnAttribute
+    private bool? _hasStringLength;         //StringLengthAttribute
+    private int? _stringMaxLength;          //StringLengthAttribute
+    private int? _stringMinLength;          //StringLengthAttribute
+    private bool? _isUrl;                   //UrlAttribute
+    private bool? _isTimeStamp;             //TimestampAttribute
+
     #endregion
 
     #region Constructors
@@ -36,92 +63,406 @@ public class EZField
 
     #endregion
 
+    public object? Value(object? context)
+    {
+        var value = _fieldInfo.GetValue(context);
+        return value;
+    }
+
     #region Public Properties
     public Type Model => _model;
     public PropertyInfo FieldInfo => _fieldInfo;
     public string FieldName => _fieldName;
-    public string Display
+
+    /// <summary>
+    /// Get the attribute value of DisplayAttribute.Name
+    /// </summary>
+    public string DisplayName
     {
         get
         {
-            DisplayAttribute? attr = (DisplayAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DisplayAttribute));
-            if (attr != null)
+            if (_displayName is null)
             {
-                return attr.Name ?? _fieldName;
+                DisplayAttribute? attr = (DisplayAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DisplayAttribute));
+                if (attr is not null) _displayName = attr?.Name;
+                if (_displayName is null) _displayName = _fieldName;
             }
-            return _fieldName;
+            return _displayName;
         }
     }
 
-    public bool Editable
+    /// <summary>
+    /// Get the attribute value of DisplayAttribute.ShortName
+    /// </summary>
+    public string DisplayShortName
     {
         get
         {
-            EditableAttribute? attr = (EditableAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(EditableAttribute));
-            if (attr != null)
+            if (_displayShortName is null)
             {
-                return attr.AllowEdit;
+                DisplayAttribute? attr = (DisplayAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DisplayAttribute));
+                if (attr is not null) _displayShortName = attr?.ShortName;
+                if (_displayShortName is null) _displayShortName = _fieldName;
             }
-            return true;
+            return _displayShortName;
         }
     }
 
-    public string? DisplayFormat
+    /// <summary>
+    /// Get the attribute value of EditableAttribute.AllowEdit
+    /// </summary>
+    public bool AllowEdit
     {
         get
         {
-            DisplayFormatAttribute? attr = (DisplayFormatAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DisplayFormatAttribute));
-            if (attr != null)
+            if (_AllowEdit is null)
             {
-                return attr.DataFormatString;
+                EditableAttribute? attr = (EditableAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(EditableAttribute));
+                if (attr is not null) _AllowEdit = attr.AllowEdit;
+                if (_AllowEdit is null) _AllowEdit = true;
             }
-            return null;
+            return _AllowEdit ?? true;
         }
     }
 
+    /// <summary>
+    /// Get the attribute value of DisplayFormatAttribute.DataFormatString
+    /// </summary>
+    public string DataFormatString
+    {
+        get
+        {
+            if (_dataFormatString is null)
+            {
+                DisplayFormatAttribute? attr = (DisplayFormatAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DisplayFormatAttribute));
+                if (attr is not null) _dataFormatString = attr.DataFormatString;
+                if (_dataFormatString is null) _dataFormatString = string.Empty;
+            }
+            return _dataFormatString;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of DisplayFormatAttribute.NullDisplayText
+    /// </summary>
+    public string NullDisplayText
+    {
+        get
+        {
+            if (_nullDisplayText is null)
+            {
+                DisplayFormatAttribute? attr = (DisplayFormatAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DisplayFormatAttribute));
+                if (attr is not null) _nullDisplayText = attr.NullDisplayText;
+                if (_nullDisplayText is null) _nullDisplayText = string.Empty;
+            }
+            return _nullDisplayText;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of KeyAttribute
+    /// </summary>
     public bool Key
     {
         get
         {
-            KeyAttribute? attr = (KeyAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(KeyAttribute));
-            if (attr != null)
-            {
-                return true;
+            if (_key is null) {
+                KeyAttribute? attr = (KeyAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(KeyAttribute));
+                if (attr is not null) _key = true;
+                else _key = false;
             }
-            return false;
+            return _key ?? false;
         }
     }
 
-    public DataType? DataType
+    /// <summary>
+    /// Get the System.Type with which the property is declared
+    /// </summary>
+    public Type PropertyType
     {
         get
         {
-            DataTypeAttribute? attr = (DataTypeAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(DataTypeAttribute));
-            if (attr != null)
-            {
-                return attr.DataType;
-            }
-            return null;
-        }
-    }
-    public bool Required
-    {
-        get
-        {
-            RequiredAttribute? attr = (RequiredAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(RequiredAttribute));
-            if (attr != null)
-            {
-                return true;
-            }
-            return false;
+            return _fieldInfo.PropertyType;
         }
     }
 
-    public Type? SystemType
+    /// <summary>
+    /// Get the attribute value of RequiredAttribute
+    /// </summary>
+    public bool IsRequired
     {
         get
         {
-            return _fieldInfo.GetType();
+            if (_isRequired is null) {
+                RequiredAttribute? attr = (RequiredAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(RequiredAttribute));
+                if (attr is not null) _isRequired = true;
+                if (_isRequired is null) _isRequired = false;
+            }
+            return _isRequired ?? false;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of CreditCardAttribute
+    /// </summary>
+    public bool IsCreditCard
+    {
+        get
+        {
+            if (_isCreditCard is null)
+            {
+                CreditCardAttribute? attr = (CreditCardAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(CreditCardAttribute));
+                if (attr is not null) _isCreditCard = true;
+                if (_isCreditCard is null) _isCreditCard = false;
+            }
+            return _isCreditCard ?? false;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of EmailAddressAttribute
+    /// </summary>
+    public bool IsEmailAddress
+    {
+        get
+        {
+            if (_isEmailAddress is null)
+            {
+                EmailAddressAttribute? attr = (EmailAddressAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(EmailAddressAttribute));
+                if (attr is not null) _isEmailAddress = true;
+                if (_isEmailAddress is null) _isEmailAddress = false;
+            }
+            return _isEmailAddress ?? false;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of PhoneAttribute
+    /// </summary>
+    public bool IsPhone
+    {
+        get
+        {
+            if (_isPhone is null)
+            {
+                PhoneAttribute? attr = (PhoneAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(PhoneAttribute));
+                if (attr is not null) _isPhone = true;
+                if (_isPhone is null) _isPhone = false;
+            }
+            return _isPhone ?? false;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of MaxLengthAttribute.Length
+    /// </summary>
+    public int? MaxLength
+    {
+        get
+        {
+            if (_hasMaxLength is null)
+            {
+                MaxLengthAttribute? attr = (MaxLengthAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(MaxLengthAttribute));
+                if (attr is not null)
+                {
+                    _hasMaxLength = true;
+                    _maxLength = attr.Length;
+                }
+                else
+                {
+                    _hasMaxLength = false;
+                    _maxLength = null;
+                }
+            }
+            return _maxLength;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of MinLengthAttribute.Length
+    /// </summary>
+    public int? MinLength
+    {
+        get
+        {
+            if (_hasMinLength is null)
+            {
+                MinLengthAttribute? attr = (MinLengthAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(MinLengthAttribute));
+                if (attr is not null)
+                {
+                    _hasMinLength = true;
+                    _minLength = attr.Length;
+                }
+                else
+                {
+                    _hasMinLength = false;
+                    _minLength = null;
+                }
+            }
+            return _minLength;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of RangeAttribute.Maximum
+    /// </summary>
+    public object? RangeMaximum
+    {
+        get
+        {
+            if (HasRange()) return _rangeMaximum;
+            else return null;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of RangeAttribute.Minimum
+    /// </summary>
+    public object? RangeMinimum
+    {
+        get
+        {
+            if (HasRange()) return _rangeMinimum;
+            else return null;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of RangeAttribute.OperandType
+    /// </summary>
+    public Type? RangeType
+    {
+        get
+        {
+            if (HasRange()) return _rangeType;
+            else return null;
+        }
+    }
+
+    private bool HasRange()
+    {
+        if (_hasRange is null)
+        {
+            RangeAttribute? attr = (RangeAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(RangeAttribute));
+            if (attr is not null)
+            {
+                _hasRange = true;
+                _rangeMaximum = attr.Maximum;
+                _rangeMinimum = attr.Minimum;
+                _rangeType = attr.OperandType;
+            }
+            else _hasRange = false;
+        }
+        return _hasRange ?? false;
+    }
+
+    /// <summary>
+    /// Get the attribute value of RegularExpressionAttribute.Pattern
+    /// </summary>
+    public string? RegularExpression {
+        get
+        {
+            if (_hasRegularExpression is null)
+            {
+                RegularExpressionAttribute? attr = (RegularExpressionAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(RegularExpressionAttribute));
+                if (attr is not null)
+                {
+                    _hasRegularExpression = true;
+                    _regularExpression = attr.Pattern;
+                }
+                else _hasRegularExpression = false;
+            }
+            return _regularExpression;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of ScaffoldColumnAttribute.Scaffold
+    /// </summary>
+    public bool ScaffoldColumn
+    {
+        get
+        {
+            if (_scaffoldColumn is null)
+            {
+                ScaffoldColumnAttribute? attr = (ScaffoldColumnAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(ScaffoldColumnAttribute));
+                if (attr is not null) _scaffoldColumn = attr.Scaffold;
+                if (_scaffoldColumn is null) _scaffoldColumn = true;
+            }
+            return _scaffoldColumn ?? true;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of StringLengthAttribute.MaximumLength
+    /// </summary>
+    public int? StringMaxLength
+    {
+        get
+        {
+            if (HasStringLength()) return _stringMaxLength;
+            else return null;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of StringLengthAttribute.MinimumLength
+    /// </summary>
+    public int? StringMinLength
+    {
+        get
+        {
+            if (HasStringLength()) return _stringMinLength;
+            else return null;
+        }
+    }
+
+    private bool HasStringLength()
+    {
+        if (_hasStringLength is null)
+        {
+            StringLengthAttribute? attr = (StringLengthAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(StringLengthAttribute));
+            if (attr is not null)
+            {
+                _stringMaxLength = attr.MaximumLength;
+                _stringMinLength = attr.MinimumLength;
+                _hasStringLength = true;
+            }
+            else _hasStringLength = false;
+        }
+        return _hasStringLength ?? false;
+    }
+
+    /// <summary>
+    /// Get the attribute value of UrlAttribute
+    /// </summary>
+    public bool IsUrl 
+    {
+        get 
+        {
+            if (_isUrl is null)
+            {
+                UrlAttribute? attr = (UrlAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(UrlAttribute));
+                if (attr is not null) _isUrl = true;
+                else _isUrl = false;
+            }
+            return _isUrl ?? false;
+        }
+    }
+
+    /// <summary>
+    /// Get the attribute value of TimestampAttribute
+    /// </summary>
+    public bool IsTimeStamp { 
+        get 
+        { 
+            if (_isTimeStamp is null)
+            {
+                TimestampAttribute? attr = (TimestampAttribute?)Attribute.GetCustomAttribute(_fieldInfo, typeof(TimestampAttribute));
+                if (attr is not null) _isTimeStamp = true;
+                else _isTimeStamp = false;
+            }
+            return _isTimeStamp ?? false;
         }
     }
 
@@ -132,23 +473,13 @@ public class EZField
 //AssociationAttribute
 //CompareAttribute
 //ConcurrencyCheckAttribute
-//CreditCardAttribute
 //CustomValidationAttribute
-//EmailAddressAttribute
 //EnumDataTypeAttribute
 //FileExtensionsAttribute
 //FilterUIHintAttribute
-//MaxLengthAttribute
 //MetadataTypeAttribute
-//MinLengthAttribute
-//PhoneAttribute
-//RangeAttribute
-//RegularExpressionAttribute
-//ScaffoldColumnAttribute
-//StringLengthAttribute
 //TimestampAttribute
 //UIHintAttribute
-//UrlAttribute
 //ValidationAttribute
 //ValidationContext
 //ValidationException
